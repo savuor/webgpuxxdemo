fa8dec5c335cfa55ee6739435e35af256c101d28
Rostislav Vasilikhin <savuor@gmail.com>
2023-07-23 01:28:53 +0200

    glfw connected

--- app.cpp
+++ app.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
+
+struct TerminatorGLFW
+{
+    ~TerminatorGLFW()
+    {
+        glfwTerminate();
+    }
+};
 
 int main (int /* argc */, char** /* argv */)
 {
-    std::cout << "Hello, world!" << std::endl;
+    if (!glfwInit())
+    {
+        std::cerr << "Could not initialize GLFW!" << std::endl;
+        return 1;
+    }
+
+    TerminatorGLFW terminatorGlfw;
+
+    // Create the window
+    GLFWwindow* window = glfwCreateWindow(640, 480, "WebGPU C++ Demo", NULL, NULL);
+
+    if (!window)
+    {
+        std::cerr << "Could not open window!" << std::endl;
+
+        return 1;
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // Check whether the user clicked on the close button (and any other
+        // mouse/key event, which we don't use so far)
+        glfwPollEvents();
+    }
+
+    // At the end of the program, destroy the window
+    glfwDestroyWindow(window);
+
     return 0;
 }
